bf735b9e2d77975d9c138451b9c70b002ecab98d
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2018-01-07 17:51:20 +0100

    try to filter issues

--- src/app/issues/issues.component.ts
+++ src/app/issues/issues.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 import { Observable } from 'rxjs/Observable';
 import { IssueService } from '../issue.service';
+import {Location} from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-issues',
@@ -13,7 +17,7 @@ export class IssuesComponent implements OnInit {
 
   issues: any[];
   selectedIssueId: string;
-  constructor(private issueService: IssueService) {}
+  constructor(private issueService: IssueService,private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.getIssues();
@@ -25,7 +29,17 @@ export class IssuesComponent implements OnInit {
   }
 
   getIssues(): void {
-    this.issueService.getIssues().subscribe(result => {
+    let par_key = '';
+    let par_value = '';
+    this.route.queryParams
+      .subscribe(params => {
+        console.log(params);
+        forEach(params, function (value, key) {
+          par_key = key;
+          par_value = value;
+        })
+      });
+    this.issueService.getIssues(par_key, par_value).subscribe(result => {
       console.log(result);
         this.issues = result.results;
     },
