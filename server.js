37adbc09689a9aa5a8117181733930a0ba396f5d
jonator <nebot8@gmail.com>
2018-01-08 11:15:16 +0100

    bug solve HEROKU

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// server.js
-const express = require('express');
+const express = require('express')
 const app = express();
-const path = require('path');
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist/app'));
-// Start the app by listening on the default
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/app/app.component.html'));
-});
-// Heroku port
+const path = require('path')
+
+app.use(express.static(__dirname + 'dist'));
+
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log('console listening');
