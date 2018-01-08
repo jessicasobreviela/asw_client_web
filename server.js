abe4051f5005a318b4330cdf9a7b4065d5e83fd9
jonator <nebot8@gmail.com>
2018-01-08 10:45:22 +0100

    bug solve HEROKU

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + 'dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/app/app.component.html'));
+  res.sendFile(path.join(__dirname + '/dist/src/app/app.component.html'));
 })
 
 console.log('console listening');
